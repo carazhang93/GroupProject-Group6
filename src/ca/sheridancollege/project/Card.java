644082bb17df06039bd3f6537104d07d71ed416b
dollar <dollar@windows10.microdone.cn>
2023-06-13 11:59:43 +0800

    Just a test

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -2,7 +2,7 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
- * Xiaoyi Zhang
+ * Xiaoyi Zhang Meichao Zhao
  */
 package ca.sheridancollege.project;
 
